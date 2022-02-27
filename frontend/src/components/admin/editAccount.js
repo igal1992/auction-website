858464bf6c1196cb12a6cac810a4e008c7c8362e
igal1992 <80173737+igal1992@users.noreply.github.com>
2022-02-28 00:15:58 +0200

    Add files via upload

---
+++ frontend/src/components/admin/editAccount.js
@@ -0,0 +1,69 @@
+import {Modal,Button} from 'react-bootstrap'; 
+import logo from '../../images/logo.jpg';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+
+function EditAccount(props){
+    const [email,setEmail] = useState('');
+    const [fullName,setFullName] = useState('');
+
+    useEffect(()=>{
+        if(props.show){
+            setEmail(props.accountToEdit.email);
+            setFullName(props.accountToEdit.fullName);
+        }
+    },[props.show])
+    
+    const updateInputs =(e) =>{
+        var value = e.target.value;
+        var name = e.target.name;
+        switch(name){
+            case 'email':
+                setEmail(value);
+                break;
+            case 'fullName':
+                setFullName(value);
+                break;
+        }
+    }
+
+    const saveAccount = () =>{
+        const newAccount = {
+            email,
+            fullName,
+            emailToEdit: props.accountToEdit.email
+        }
+        if(email!==''&& fullName!==''){
+            axios.post('http://localhost:8080/user/edit',{newAccount})
+            .then(res=>{
+                props.setShow(false);
+                axios.get('http://localhost:8080/user/getAll').then(res=>{
+                    props.updateAccounts(res.data);
+                })
+            }).catch(err=>{
+                console.log(err);
+            })
+        }   
+    }
+    return(
+    <Modal show={props.show} size="md" centered  className='pop-screen-container' animation={false}>
+        <Modal.Header>
+          <Modal.Title className='pop-screen-header'>
+            <img src={logo}/>
+            <h1>Edit Account</h1>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form className='pop-screen-body'>
+            <input placeholder='email' name = {'email'} onChange={updateInputs} value={email}/>
+            <input placeholder='fullName' name = {'fullName'} onChange={updateInputs} value={fullName}/>
+          </form>
+        </Modal.Body>
+        <Modal.Footer className='pop-screen-footer'>
+          <Button variant="success" onClick={saveAccount}>Save</Button>
+          <Button variant="danger" onClick={()=>{props.setShow(false)}}>Close</Button>
+        </Modal.Footer>
+    </Modal>
+    );
+}
+export default EditAccount;
