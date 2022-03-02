87ba5d614eab530c3ba857aa5ae695976e7cf8b1
igal1992 <80173737+igal1992@users.noreply.github.com>
2022-03-02 16:30:00 +0200

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Home from './components/home';
+import Auction from './components/auction/auction';
+import TopNav from './components/topNav';
+import Admin from './components/admin/admin';
+import MyAccount from './components/account/myAccount';
+import {Route,Routes} from 'react-router-dom';
+
+
+
+function App() {
+  return (
+    <div>
+      <TopNav />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/auction" element={<Auction />}/>
+        <Route path='/admin' element={<Admin />}/>
+        <Route path='/myAccount' element={<MyAccount />}/>
+      </Routes>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ frontend/src/components/auction/rowProducts.js
@@ -0,0 +1,34 @@
+import {useSelector} from 'react-redux';
+import logo from '../../images/logo.jpg';
+import { imageExists} from '../utils';
+import imgNotFound from '../../images/imgNotFound.png';
+import {Button,Spinner} from 'react-bootstrap';
+
+function RowProducts(props){
+    const filteredProducts = useSelector(state =>state.filteredProducts);
+    return(
+        <ul id ={props.id}>
+              {props.loaded && typeof filteredProducts[0] === 'object' && Object.keys(filteredProducts[0]).length !== 0?props.currentProducts.map((product)=>{
+                  return(
+                    <li className={'product-row-outer'} key={product.title}>
+                        <img className={'product-row-image'} src={imageExists(product.image)?product.image:imgNotFound}/>
+                        <div className={'product-row'} >
+                            <div className={'product-row-body'}>
+                                <h2 className={'product-row-title'}>{product.title}</h2>
+                                <div className={'product-row-text'}>
+                                {product.description}<br/>
+                                <strong>{product.uploader}</strong><br/>
+                                </div>
+                            </div>
+                            <div className={'product-row-button-container'}>
+                                <div className={'row-product-button-inner'}><strong>{product.price} $</strong></div>
+                                <div className={'row-product-button-inner'}><Button variant="success" className={'product-row-add'} onClick={()=>{props.addToCartProduct(product)}}>Add to Cart</Button> <Button variant="success" className={'product-row-buy'} onClick={()=>{props.instantBuy(product)}}>Buy</Button></div>
+                            </div>
+                        </div>
+                    </li>
+                  )
+              }):<div id={'products-loader-container'}><Spinner animation="border" variant="dark" id={'products-loader'} /></div>}
+        </ul>
+    )
+}
+export default RowProducts;
